35ca45d902942e746e2ddae1f85b173f5639f61a
rubenavaldez <48387594+rubenavaldez@users.noreply.github.com>
2019-06-22 11:58:39 -0700

    ajax test

--- videoSearch.js
+++ videoSearch.js
@@ -26,74 +26,19 @@ var userChoice = "portal";
 $("body").on("click", "#search", function () {
   console.log("Click")
 
-  axios.get("http://www.giantbomb.com/search" , {
+  $.ajax({
+    url: "http://api.giantbomb.com/search/",
     dataType: "jsonp",
-                    jsonp: 'json_callback',
-                    data: {
-                        api_key: '[e7b9ce5f17b926a054c14d54e4e5c5ef2cb2fed8]',
-                        query: 'mass effect',
-                        format: 'jsonp',
-                        field_list: 'name'
-      
+    jsonp: 'json_callback',
+    data: {
+        api_key: '[e7b9ce5f17b926a054c14d54e4e5c5ef2cb2fed8]',
+        query: 'mass effect',
+        format: 'jsonp',
+        field_list: 'name'
+    },
+    success: function(res) {
+        console.log(res);
     }
-  })
-    .then(response => {
-      // Do work here
-      console.log(response)
-      console.log(response.data);
-      console.log(response.data.result.title);
-      console.log(response.data.result.description);
-      console.log(response.data.result.image);
-      console.log(response.data.result.genre);
-      console.log(response.data.result.score);
-
-      var cardDiv = $("<div>");
-      cardDiv.addClass("card");
-
-      var cardImage = $("<img>");
-      cardImage.attr("class","card-image-top");
-      cardImage.attr("src",response.data.result.image);
-
-      var cardBody = $("<div>");
-      cardBody.attr("class","card-body");
-
-      var cardTitle = $("<h5>").text(response.data.result.title);
-      cardTitle.attr("class","card-title");
-
-      var cardDescribe = $("<p>").text(response.data.result.description);
-      cardDescribe.attr("class","card-text");
-
-      var cardFooter1 = $("<p>");
-      cardFooter1.attr("class","card-text");
-
-      var cardFooter2 = $("<p>");
-      cardFooter2.attr("class","card-text");
-
-      var cardGenre = $("<small>").text(response.data.result.genre);
-      cardGenre.attr("class", "text-muted");
-
-      var cardScore = $("<small>").text("Score: " + response.data.result.score);
-      cardScore.attr("class", "text-muted");
-
-      cardFooter1.append(cardGenre);
-
-      cardFooter2.append(cardScore);
-      
-      cardBody.append(cardTitle);
-      cardBody.append(cardDescribe);
-      cardBody.append(cardFooter1);
-      cardBody.append(cardFooter2);
-
-      cardDiv.append(cardImage);
-      cardDiv.append(cardBody);
-
-      $("#card-deck").prepend(cardDiv);
-      
-      
-    })
-    .catch(e => {
-      console.log("error", e);
-    });
-
 });
-
+});
+    
