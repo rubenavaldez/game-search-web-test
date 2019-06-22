3e45a69989c5343790b28a1e4fcc693f14a2b8a0
rubenavaldez <48387594+rubenavaldez@users.noreply.github.com>
2019-06-22 11:47:06 -0700

    giant bomb test

--- videoSearch.js
+++ videoSearch.js
@@ -17,29 +17,83 @@ $(document).ready(function () {
 
 });
 
-var userChoice = "portal"
+var platform = "pc"
 
+var userChoice = "portal";
+// + userChoice + "?platform=" + platform 
+//api_key=[e7b9ce5f17b926a054c14d54e4e5c5ef2cb2fed8
 
 $("body").on("click", "#search", function () {
   console.log("Click")
 
-  axios.get("https://chicken-coop.p.rapidapi.com/games/" + userChoice + "?platform=pc", {
-    headers: {
-      'Access-Control-Allow-Origin': 'https://www.igdb.com/oauth/authorize',
-      
-       "X-RapidAPI-Host": "chicken-coop.p.rapidapi.com",
-      "X-RapidAPI-Key": "09840eae9fmsh29e1cf0c587e7d6p114d45jsnfbc066e65e3c",
-
-      Accept: "application/json"
+  axios.get("http://www.giantbomb.com/search" , {
+    dataType: "jsonp",
+                    jsonp: 'json_callback',
+                    data: {
+                        api_key: '[e7b9ce5f17b926a054c14d54e4e5c5ef2cb2fed8]',
+                        query: 'mass effect',
+                        format: 'jsonp',
+                        field_list: 'name'
       
     }
   })
     .then(response => {
       // Do work here
+      console.log(response)
       console.log(response.data);
+      console.log(response.data.result.title);
+      console.log(response.data.result.description);
+      console.log(response.data.result.image);
+      console.log(response.data.result.genre);
+      console.log(response.data.result.score);
+
+      var cardDiv = $("<div>");
+      cardDiv.addClass("card");
+
+      var cardImage = $("<img>");
+      cardImage.attr("class","card-image-top");
+      cardImage.attr("src",response.data.result.image);
+
+      var cardBody = $("<div>");
+      cardBody.attr("class","card-body");
+
+      var cardTitle = $("<h5>").text(response.data.result.title);
+      cardTitle.attr("class","card-title");
+
+      var cardDescribe = $("<p>").text(response.data.result.description);
+      cardDescribe.attr("class","card-text");
+
+      var cardFooter1 = $("<p>");
+      cardFooter1.attr("class","card-text");
+
+      var cardFooter2 = $("<p>");
+      cardFooter2.attr("class","card-text");
+
+      var cardGenre = $("<small>").text(response.data.result.genre);
+      cardGenre.attr("class", "text-muted");
+
+      var cardScore = $("<small>").text("Score: " + response.data.result.score);
+      cardScore.attr("class", "text-muted");
+
+      cardFooter1.append(cardGenre);
+
+      cardFooter2.append(cardScore);
+      
+      cardBody.append(cardTitle);
+      cardBody.append(cardDescribe);
+      cardBody.append(cardFooter1);
+      cardBody.append(cardFooter2);
+
+      cardDiv.append(cardImage);
+      cardDiv.append(cardBody);
+
+      $("#card-deck").prepend(cardDiv);
+      
+      
     })
     .catch(e => {
       console.log("error", e);
     });
 
 });
+
